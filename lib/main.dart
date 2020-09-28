464d4bea36ab36bd2b9f367f35dd73b8ee5aa3e6
jaspreetsidhu3 <jaspreetsidhu3may@gmail.com>
2020-09-28 14:00:00 +0100

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,64 @@
     08/09/2020
  */
 
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'Screens/HomeActivity.dart';
 
 // main function where the execution starts
 void main() {
   // inflating the widgets to views
-  runApp(MyMaterialApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blueAccent,
+        backgroundColor: Colors.deepPurple,
+        brightness: Brightness.dark,
+      ),
+      title: "BMI Calculator",
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return SplashScreenState();
+  }
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+            () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => MyMaterialApp())));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
+        body: Center(
+          child: Text("Bmi Calculator",textAlign: TextAlign.left,style: TextStyle(color: Colors.blue,fontSize: 21),),
+        ),
+      )
+
+
+    );
+    // TODO: implement build
+
+  }
 }
 
 // Building all the required stateless widgets
